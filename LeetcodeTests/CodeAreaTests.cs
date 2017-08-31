132c1d3c9c222b60dea0f1d194f113b0739e242c
ecohover <ecohover@gmail.com>
2017-08-31 14:35:15 +0800

    first push

---
+++ LeetcodeTests/CodeAreaTests.cs
@@ -0,0 +1,414 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Tests
+{
+    [TestClass()]
+    public class CodeAreaTests
+    {
+        [TestMethod()]
+        public void TwoSumTest()
+        {
+            CodeArea target = new CodeArea();
+            int[] num = new int[] { 2, 7, 11, 15 };
+            int[] expected = new int[] { 0, 1 };
+            int[] output = target.TwoSum(num, 9);
+            Assert.AreEqual(expected, output);
+        }
+
+        [TestMethod()]
+        public void AddTwoNumbersTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void LengthOfLongestSubstringTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void FindMedianSortedArraysTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void LongestPalindromeTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void CheckPalindromeTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Convert2Test()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Convert1Test()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ReverseTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Reverse2Test()
+        {
+
+        }
+
+        [TestMethod()]
+        public void MyAtoiTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void CheckIntTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void CheckSignTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void IsPalindromeTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void IsMatchTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void MaxAreaTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void IntToRomanTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void RomanToIntTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void RomanToInt2Test()
+        {
+
+        }
+
+        [TestMethod()]
+        public void LongestCommonPrefix3Test()
+        {
+
+        }
+
+        [TestMethod()]
+        public void LongestCommonPrefixTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ThreeSumTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void AddListTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ThreeSumClosestTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void RemoveNthFromEndTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void IsValidTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void MergeTwoListsTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void SwapPairsTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void RemoveDuplicatesTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void RemoveElementTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void StrStrTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void IsValidSudokuTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void CountAndSayTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void LengthOfLastWordTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void PlusOneTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void AddBinaryTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ClimbStairsTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void DeleteDuplicatesTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void MergeTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void IsSameTreeTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void IsSameTree2Test()
+        {
+
+        }
+
+        [TestMethod()]
+        public void IsSymmetricTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void IsSymmetricTest1()
+        {
+
+        }
+
+        [TestMethod()]
+        public void IsSymmetric2Test()
+        {
+
+        }
+
+        [TestMethod()]
+        public void LevelOrderTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void AddListTest1()
+        {
+
+        }
+
+        [TestMethod()]
+        public void MaxDepthTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetMaxTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void LevelOrderBottomTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void OrderBottomTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void IsBalancedTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void MinDepthTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetMinTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void HasPathSumTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GenerateTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetNextLevelTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetRowTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void MaxProfitTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void IsPalindromeTest1()
+        {
+
+        }
+
+        [TestMethod()]
+        public void SingleNumberTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void HasCycleTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetIntersectionNodeTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void IsUglyTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void CheckTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void NthUglyNumberTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void MinTest()
+        {
+
+        }
+    }
+}
